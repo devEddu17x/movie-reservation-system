d915f82a6288e282ec2d6233ed1aef197762f073
Eddu <azabacheeduardo85@gmail.com>
2024-12-08 19:48:06 -0500

    feat: configure NestJS logger options in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
   // get configService
   const configService = app.get(ConfigService);
   const PORT = configService.get('PORT');
