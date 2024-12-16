a0db412f6c358a2180314f163c7e05f3b4c58036
Eddu <azabacheeduardo85@gmail.com>
2024-12-16 18:11:12 -0500

    feat: add role-based access control to movie creation endpoint

--- src/movie/controllers/movie.controller.ts
+++ src/movie/controllers/movie.controller.ts
@@ -1,11 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { MovieService } from '../services/movie.service';
 import { CreateMovieDTO } from '../dtos/create-movie.dto';
+import { Roles } from 'src/core/shared/decorators/roles.decorator';
+import { RoleType } from 'src/user/enums/role-type.enum';
+import { RolesGuard } from 'src/core/shared/guards/roles.guard';
 
 @Controller('movie')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
   @Post()
+  @UseGuards(RolesGuard)
+  @Roles(RoleType.ADMIN)
   async createMovie(@Body() movie: CreateMovieDTO) {
     const createdMovie = await this.movieService.createMovie(movie);
     return {
