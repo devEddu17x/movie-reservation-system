c1537c1a5116ecb90b64b07049210fd64b7e6c1e
Eddu <azabacheeduardo85@gmail.com>
2025-01-13 21:23:12 -0500

    refactor: remove IsDate validator and update message for future date validation in CreateShowtimeDto

--- src/showtime/dtos/create-showtime.dto.ts
+++ src/showtime/dtos/create-showtime.dto.ts
@@ -1,13 +1,12 @@
-import { IsDate, IsNotEmpty, IsUUID, Max, Min } from 'class-validator';
+import { IsNotEmpty, IsUUID, Max, Min } from 'class-validator';
 import { IsFutureDate } from 'src/utils/future-dates.custom-validator';
 import { HasTimePrecision } from 'src/utils/has-time-precision.custom-validator';
 
 export class CreateShowtimeDto {
   @IsNotEmpty()
-  @IsDate()
   @HasTimePrecision()
   @IsFutureDate(8, {
-    message: 'The start date must be at least 8 days in the future.',
+    message: 'The startDate must be at least 8 hours in the future.',
   })
   startDate: Date;
 
@@ -17,6 +16,6 @@ export class CreateShowtimeDto {
   seatPrice: number;
 
   @IsNotEmpty()
-  @IsUUID('4', { message: 'This field must refer to a valid movie.' })
+  @IsUUID()
   movieId: string;
 }
