21039cb8c292cb26df609c921abedfadb3f3c156
Eddu <azabacheeduardo85@gmail.com>
2025-01-14 16:30:58 -0500

    feat: add JWT and role-based guards to RoleController for enhanced security

--- src/user/controllers/role.controller.ts
+++ src/user/controllers/role.controller.ts
@@ -1,8 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { RoleService } from '../services/role.service';
 import { CreateRoleDTO } from '../dtos/create-role.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Roles } from 'src/core/shared/decorators/roles.decorator';
+import { RolesGuard } from 'src/core/shared/guards/roles.guard';
+import { RoleType } from '../enums/role-type.enum';
 
 @Controller('role')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(RoleType.ADMIN)
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
   @Post()
