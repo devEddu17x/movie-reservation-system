85d36bcb5c559b1e0a109a16a58644d757fcd3e7
Eddu <azabacheeduardo85@gmail.com>
2025-01-16 20:59:14 -0500

    refactor: remove UpdateRoomDTO as it is no longer needed

--- src/room/dto/update-room.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRoomDto } from './create-room.dto';
-
-export class UpdateRoomDTO extends PartialType(CreateRoomDto) {
-  blockUntil?: Date;
-}
