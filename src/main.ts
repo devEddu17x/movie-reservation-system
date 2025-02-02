f8e033a1ec17a789d9968307e642ed0f348a4dac
Eddu <azabacheeduardo85@gmail.com>
2025-02-02 09:32:39 -0500

    feat: add morgan middleware for HTTP request logging

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-
+import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
@@ -17,6 +17,7 @@ async function bootstrap() {
     origin: true, // this must be changed to the frontend domain
     credentials: true,
   });
+  app.use(morgan('dev'));
   await app.listen(PORT || 3000);
   console.log(`Application is running on: http://localhost:${PORT}`);
 }
