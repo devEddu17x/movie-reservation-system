0254b4e557f472b7f1cbd6ae9fa5e608565d7f2f
Eddu <azabacheeduardo85@gmail.com>
2025-02-06 19:32:25 -0500

    feat: add PayPal webhook controller and configure raw body middleware for webhook route

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,12 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { PaymentController } from './controllers/payment.controller';
-import { PaymentService } from './payment.service';
+import { PaymentService } from './services/payment.service';
 import { ReservationModule } from 'src/reservation/reservation.module';
 import { SharedModule } from 'src/core/shared/shared.module';
+import { RawBodyMiddleware } from 'src/core/shared/middlewares/raw-body.middleware';
+import { PaypalWebhookController } from './controllers/paypal-webhook.controller';
 
 @Module({
   imports: [SharedModule, ReservationModule],
-  controllers: [PaymentController],
+  controllers: [PaymentController, PaypalWebhookController],
   providers: [PaymentService],
 })
-export class PaymentModule {}
+export class PaymentModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RawBodyMiddleware)
+      .forRoutes({ path: 'webhook/paypal', method: RequestMethod.POST });
+  }
+}
