ecbb0d157af0928ba60eed0db7c79d9ee8cdbd85
Eddu <azabacheeduardo85@gmail.com>
2025-02-10 21:00:22 -0500

    feat: add logging for confirmed reservations in HandleWebhooksEventsService

--- src/payment/services/handle-webhooks-events.service.ts
+++ src/payment/services/handle-webhooks-events.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PaypalWebHookEvents } from '../enums/webhook.events.enum';
 import { ReservationService } from 'src/reservation/services/reservation.service';
 
 @Injectable()
 export class HandleWebhooksEventsService {
+  private readonly logger = new Logger(HandleWebhooksEventsService.name);
   constructor(private readonly reservationService: ReservationService) {}
   async handleEvent(eventType: string, data: any) {
     switch (eventType) {
@@ -16,6 +17,7 @@ export class HandleWebhooksEventsService {
     }
   }
   private async handleCaptureEvent(reservationId: string) {
+    this.logger.log(`Reservation ${reservationId} confirmed`);
     await this.reservationService.confirmReservation(reservationId);
   }
   private async handleDeniedEvent() {}
